288a15a681821ff8734dd3fff3c4cbcf78e4dd53
kylejuthan <kylejuthan@hotmail.com>
2017-12-06 23:36:12 +0000

    Added education and partial work experience

--- js/scripts.js
+++ js/scripts.js
@@ -1,55 +1,54 @@
-(function($) {
-    "use strict"; // Start of use strict
-  
-    // Smooth scrolling using jQuery easing
-    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
-      if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-        var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-        if (target.length) {
-          $('html, body').animate({
-            scrollTop: (target.offset().top - 54)
-          }, 1000, "easeInOutExpo");
-          return false;
-        }
+(function ($) {
+  "use strict"; // Start of use strict
+
+  // Smooth scrolling using jQuery easing
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: (target.offset().top - 54)
+        }, 1000, "easeInOutExpo");
+        return false;
       }
-    });
-  
-    // Closes responsive menu when a scroll trigger link is clicked
-    $('.js-scroll-trigger').click(function() {
-      $('.navbar-collapse').collapse('hide');
-    });
-  
-    // Activate scrollspy to add active class to navbar items on scroll
-    $('body').scrollspy({
-      target: '#myNav',
-      offset: 54
-    });
-  
-  })(jQuery); // End of use strict and end of scroll trigger
-
-
-  function submitContactForm(){
-    var reg = /^[A-Z0-9._%+-]+@([A-Z0-9-]+\.)+[A-Z]{2,4}$/i;
-    var name = $('#inputName').val();
-    var email = $('#inputEmail').val();
-    var message = $('#inputMessage').val();
-    if(name.trim() == '' ){
-        alert('Please enter your name.');
-        $('#inputName').focus();
-        return false;
-    }else if(email.trim() == '' ){
-        alert('Please enter your email.');
-        $('#inputEmail').focus();
-        return false;
-    }else if(email.trim() != '' && !reg.test(email)){
-        alert('Please enter valid email.');
-        $('#inputEmail').focus();
-        return false;
-    }else if(message.trim() == '' ){
-        alert('Please enter your message.');
-        $('#inputMessage').focus();
-        return false;
     }
+  });
 
+  // Closes responsive menu when a scroll trigger link is clicked
+  $('.js-scroll-trigger').click(function () {
+    $('.navbar-collapse').collapse('hide');
+  });
+
+  // Activate scrollspy to add active class to navbar items on scroll
+  $('body').scrollspy({
+    target: '#myNav',
+    offset: 54
+  });
+
+})(jQuery); // End of use strict and end of scroll trigger
+
+
+function submitContactForm() {
+  var reg = /^[A-Z0-9._%+-]+@([A-Z0-9-]+\.)+[A-Z]{2,4}$/i;
+  var name = $('#inputName').val();
+  var email = $('#inputEmail').val();
+  var message = $('#inputMessage').val();
+  if (name.trim() == '') {
+    alert('Please enter your name.');
+    $('#inputName').focus();
+    return false;
+  } else if (email.trim() == '') {
+    alert('Please enter your email.');
+    $('#inputEmail').focus();
+    return false;
+  } else if (email.trim() != '' && !reg.test(email)) {
+    alert('Please enter valid email.');
+    $('#inputEmail').focus();
+    return false;
+  } else if (message.trim() == '') {
+    alert('Please enter your message.');
+    $('#inputMessage').focus();
+    return false;
+  }
 }
